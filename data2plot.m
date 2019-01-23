96f155a208a4e00cacf1e40c7e3d9e4e669b4322
bastamon <bastamon@sohu.com>
2019-01-23 16:48:00 +0800

    add circuit vector

--- data2plot.m
+++ data2plot.m
@@ -10,10 +10,20 @@ for i=1:length(ref)
     s=char(char2asc2);
     ref(i)=str2double(s);
 end
+I=zeros(size(ref));
+for i=1:length(ref)
+    s=cell2mat(temp(i,2));
+    s(isspace(s)) = [];
+    char2asc2=abs(s);%转asc2
+    char2asc2(char2asc2==abs('A'))=[];%删除5
+    s=char(char2asc2);
+    I(i)=str2double(s);
+end
+ref=[ref I];
 save ( 'volt.mat', 'ref');clear;
 load volt;
 figure('NumberTitle', 'off', 'Name', 'energytrend');
-plot(ref);
+plot(ref(:,1));
 xlim([0 1e5]);
 hold on;
 title('电压变化趋势');
